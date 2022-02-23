59c217ea891402a9387da88020e2111ccebaf909
Juan Rodriguez <vengeance801@gmail.com>
2022-02-23 11:40:23 -0600

    Cambios clase 41

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,6 @@ import { Component } from '@angular/core';
   // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titulo: string = 'Contador App';
-  numero: number = 0;
-  base  : number = 5;
-
-  // sumar(){
-  //   this.numero++;
-  // }
-
-  // restar(){
-  //   this.numero--;
-  // }
-
-  acumular(valor: number){
-    this.numero += valor;
-  }
+  
 
 }
